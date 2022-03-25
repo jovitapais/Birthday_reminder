df986ccedb45f075c18fb5ed4efd46043cdb1d59
Jovita <jovitaspais@gmail.com>
2022-03-25 18:34:25 -0400

    set up static data

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
   return <main>
     <section className="container">
       <h3>0 birthdays today</h3>
+      <List/>
+      <button onClick={() => {}}>clear all</button>
     </section>
   </main>;
 }
